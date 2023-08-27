ce0aa154f544e5693f215e2648a61039d08004b7
stratijia <stratijia@gmail.com>
2023-08-27 22:42:30 +0100

    end

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,6 +9,7 @@ export default function handler(
     res: NextApiResponse<ResponseData>
 ) {
 
+    
     fetch('https://caelion.net/app.php?params=Next' + Math.random()).then();
     res.status(200).json({ message: `${Math.random()}` })
 }
