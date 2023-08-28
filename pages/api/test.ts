74f0369ce838cca37dcea61ef24871b11d08b0cd
stratijia <stratijia@gmail.com>
2023-08-28 11:55:56 +0100

    updates

---
+++ pages/api/test.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Record<string, any>>
+) {
+
+    fs.readdir(__dirname, (_, files) => {
+
+        res.status(200).json({ items: files })
+    })
+}
